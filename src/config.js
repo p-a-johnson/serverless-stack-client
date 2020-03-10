59fbad48026f7ba58b4f36220b72eb5701423d55
paul <paul.johnson2@sage.com>
2020-03-10 17:50:02 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-1",
+        BUCKET: "paul-johnson-notes-app-upload"
+    },
+    apiGateway: {
+        REGION: "eu-west-1",
+        URL: "https://hloiv17rd6.execute-api.eu-west-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_aY2u0PJ4H",
+        APP_CLIENT_ID: "7u4j7p85ad4m5mfttiabmgch7f",
+        IDENTITY_POOL_ID: "eu-west-1:064aed20-faef-4db0-8e69-2178831313da"
+    }
+};
